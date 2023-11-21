b3ee3d1996a2a5545a7a9030006e0ef54da4e727
fptcsm <gianglamweb@gmail.com>
2023-11-21 11:53:57 +0700

    commit

--- src/components/LanguageSwitcher.js
+++ src/components/LanguageSwitcher.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { setLanguageToLocalStorage } from '../i18n';
+import vnflag from "../assets/flag/vn.png"
+import enflag from "../assets/flag/en.png"
 
 function LanguageSwitcher() {
   const { i18n, t } = useTranslation();
@@ -23,14 +25,15 @@ function LanguageSwitcher() {
       <div className="select-wrapper">
         <div className="select" onClick={toggleOptions}>
           {i18n.language === 'en' ? 'English' : 'Tiếng Việt'}
+          {i18n.language === 'en' ? <img src={enflag} alt="" /> : <img src={vnflag} alt="" />}
         </div>
         {showOptions && (
           <div className="option-list">
             <div className="option" onClick={() => changeLanguage('en')}>
-              {t("en")}
+              {t("en")} <img src={enflag} alt="" />
             </div>
             <div className="option" onClick={() => changeLanguage('vi')}>
-              {t("vi")}
+              {t("vi")} <img src={vnflag} alt="" />
             </div>
             {/* Add more language options as needed */}
           </div>
